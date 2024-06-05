710be7416079e49bbf875ad72016ed0e92b3eefc
king-1-9 <abod46071@gmail.com>
2024-06-05 12:55:20 +0300

    سورس الملك ميوزك

---
+++ ZeMusic/plugins/bot/start.py
@@ -0,0 +1,142 @@
+import time
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtubesearchpython.__future__ import VideosSearch
+
+import config
+from ZeMusic import app
+from ZeMusic.misc import _boot_
+from ZeMusic.plugins.sudo.sudoers import sudoers_list
+from ZeMusic.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_lang,
+    is_banned_user,
+    is_on_off,
+)
+from ZeMusic.utils.decorators.language import LanguageStart
+from ZeMusic.utils.formatters import get_readable_time
+from ZeMusic.utils.inline import help_pannel, private_panel, start_panel
+from config import BANNED_USERS
+from strings import get_string
+
+
+@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+@LanguageStart
+async def start_pm(client, message: Message, _):
+    await add_served_user(message.from_user.id)
+    if len(message.text.split()) > 1:
+        name = message.text.split(None, 1)[1]
+        if name[0:4] == "help":
+            keyboard = help_pannel(_)
+            return await message.reply_photo(
+                photo=config.START_IMG_URL,
+                caption=_["help_1"].format(config.SUPPORT_CHAT),
+                reply_markup=keyboard,
+            )
+        if name[0:3] == "sud":
+            await sudoers_list(client=client, message=message, _=_)
+            if await is_on_off(2):
+                return await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"<a href='tg://user?id={message.from_user.id}'>{message.from_user.mention}</a> ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                )
+            return
+        if name[0:3] == "inf":
+            m = await message.reply_text("🎸")
+            query = (str(name)).replace("info_", "", 1)
+            query = f"https://www.youtube.com/watch?v={query}"
+            results = VideosSearch(query, limit=1)
+            for result in (await results.next())["result"]:
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
+                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+                channellink = result["channel"]["link"]
+                channel = result["channel"]["name"]
+                link = result["link"]
+                published = result["publishedTime"]
+            searched_text = _["start_6"].format(
+                title, duration, views, published, channellink, channel, app.mention
+            )
+            key = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text=_["S_B_8"], url=link),
+                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
+                    ],
+                ]
+            )
+            await m.delete()
+            await app.send_photo(
+                chat_id=message.chat.id,
+                photo=thumbnail,
+                caption=searched_text,
+                reply_markup=key,
+            )
+            if await is_on_off(2):
+                return await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"<a href='tg://user?id={message.from_user.id}'>{message.from_user.mention}</a> ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                )
+    else:
+        if await is_on_off(2):
+            return await app.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"<a href='tg://user?id={message.from_user.id}'>{message.from_user.mention}</a> قام بالدخول للبوت.\n\n<b>الايدي :</b> <code>{message.from_user.id}</code>\n<b>اليوزر :</b> @{message.from_user.username}",
+            )
+
+
+@app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
+@LanguageStart
+async def start_gp(client, message: Message, _):
+    out = start_panel(_)
+    uptime = int(time.time() - _boot_)
+    await message.reply_photo(
+        photo=config.START_IMG_URL,
+        caption=f"""منور ببوت الحمايه والتسليه وتشغيل الصوتيات بالمكالمه🤌🏻\n\n<a href='https://t.me/AAAzeo'>قناه اذكار🌿.</a>""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="أضفني إلى مجموعتك",
+                        url=f"https://t.me/{app.username}?startgroup=true",)
+                ],
+            ]
+        ),)
+    return await add_served_chat(message.chat.id)
+
+
+@app.on_message(filters.new_chat_members, group=-1)
+async def welcome(client, message: Message):
+    for member in message.new_chat_members:
+        try:
+            language = await get_lang(message.chat.id)
+            _ = get_string(language)
+            if await is_banned_user(member.id):
+                try:
+                    await message.chat.ban_member(member.id)
+                except:
+                    pass
+            if member.id == app.id:
+                #if message.chat.type != ChatType.SUPERGROUP:
+                    #await message.reply_text(_["start_4"])
+                    #return await app.leave_chat(message.chat.id)
+                if message.chat.id in await blacklisted_chats():
+                    await message.reply_text(
+                        _["start_5"].format(
+                            app.mention,
+                            f"https://t.me/{app.username}?start=sudolist",
+                            config.SUPPORT_CHAT,
+                        ),
+                        disable_web_page_preview=True,
+                    )
+                    return await app.leave_chat(message.chat.id)
+                    
+                await add_served_chat(message.chat.id)
+                await message.stop_propagation()
+        except Exception as ex:
+            print(ex)
---
+++ ZeMusic/plugins/play/دعوه.py
@@ -0,0 +1,34 @@
+
+from config import OWNER_ID
+import asyncio
+from pyrogram import Client, filters
+from ZeMusic.utils.database import get_assistant
+from pyrogram.types import Message
+from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from ZeMusic.core.call import Mody
+
+
+@app.on_message(filters.video_chat_started)
+async def brah(_, msg):
+       await msg.reply("<b>‹ تم بديت المكالمه ادخلو نسمع صوتكم ☕️.›</b>")
+
+
+@app.on_message(filters.video_chat_ended)
+async def brah2(_, msg):
+       await msg.reply("<b>‹ طفت المكالمه ماراح نسمع اصواتكم 🙇🏻. ›</b>")
+
+
+@app.on_message(filters.video_chat_members_invited)
+async def brah3(app :app, message:Message):
+           text = f"↞ هالحلو {message.from_user.mention} \n↞ يبيك للمكالمه :"
+           x = 0
+           for user in message.video_chat_members_invited.users:
+             try:
+               text += f"<a href='tg://user?id={user.id}'>{user.first_name}</a>"
+               x += 1
+             except Exception:
+               pass
+           try:
+             await message.reply(f"{text}")
+           except:
+             pass
